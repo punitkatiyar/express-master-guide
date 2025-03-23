902afb1f4e66a69440f7dba904c58c7c724367d0
Punit Katiyar <punit.kat@gmail.com>
2025-03-23 13:38:35 +0530

    Update 🎲

--- 07.express-mongodb/index.js
+++ 07.express-mongodb/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
